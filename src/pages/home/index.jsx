b1d2d364431d06b85f37a8b1dd02cf526ae4ff29
Christian De Leon <christiandeleon12693@gmail.com>
2022-07-21 14:28:13 -0400

    finished aws certs section

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Infra1 from "../../assets/images/infra-1.png";
+import AWSCerts from "../../assets/images/aws-certs.png";
 import "./style.css";
 
 class Home extends Component {
@@ -21,27 +22,78 @@ class Home extends Component {
         <section className="block">
           <div className="container grid grid--1x2 about-container">
             <div className="about__content">
-              <h2 className="about__heading">LET ME <span className="heading-highlight">INTRODUCE</span> MYSELF</h2>
+              <h2 className="about__heading">
+                LET ME <span className="heading-highlight">INTRODUCE</span>{" "}
+                MYSELF
+              </h2>
               <p>
-                I'm on a journey to master Cloud Computing, DevOps, and
-                Software Development.
+                I'm on a journey to master Cloud Computing, DevOps, and Software
+                Development.
                 <br />
                 <br />
-                My interest in cloud computing stemmed
-                from a desire to build large complex efficient systems. 
+                My interest in cloud computing stemmed from a desire to build
+                large complex efficient systems.
                 <br />
                 <br />
-                I believe in working above the baselivne of expectation by always
-                pushing myself to learn new and complex systems and
+                I believe in working above the baselivne of expectation by
+                always pushing myself to learn new and complex systems and
                 technologies.
                 <br />
                 <br />
-                Apart from Cloud Computing, I have a background in
-                Mechanical and Electrical Engineering as well as Software
-                Development.
+                Apart from Cloud Computing, I have a background in Mechanical
+                and Electrical Engineering as well as Software Development.
               </p>
             </div>
-            <img src={Infra1} alt="" className="intro-image" />
+            <img src={Infra1} alt="" className="about-image" />
+          </div>
+
+          <div className="container grid grid--1x2 about-container">
+            <img src={AWSCerts} alt="" className="about-image" />
+            <div className="about__content">
+              <h2 className="about__heading">
+                AWS <span className="heading-highlight">CERTIFIED</span>
+              </h2>
+              <p>
+                I currently hold five active AWS Certifications and I'm
+                currently preparing for my Solutions Architect Professional.
+                <br />
+                <br />
+                With a good understanding of all AWS services, I have an
+                extensive amount of exprience with the following products:
+              </p>
+              <div className="aws-services__list">
+                <ul className="aws-services">
+                  <li>CloudFormation</li>
+                  <li>CDK</li>
+                  <li>CodeBuild</li>
+                  <li>CodeCommit</li>
+                  <li>CodeDeploy</li>
+                  <li>CodePipeline</li>
+                  <li>CloudWatch</li>
+                  <li>EC2</li>
+                </ul>
+                <ul className="aws-services">
+                  <li>CloudFormation</li>
+                  <li>CDK</li>
+                  <li>CodeBuild</li>
+                  <li>CodeCommit</li>
+                  <li>CodeDeploy</li>
+                  <li>CodePipeline</li>
+                  <li>CloudWatch</li>
+                  <li>EC2</li>
+                </ul>
+                <ul className="aws-services">
+                  <li>CloudFormation</li>
+                  <li>CDK</li>
+                  <li>CodeBuild</li>
+                  <li>CodeCommit</li>
+                  <li>CodeDeploy</li>
+                  <li>CodePipeline</li>
+                  <li>CloudWatch</li>
+                  <li>EC2</li>
+                </ul>
+              </div>
+            </div>
           </div>
 
           <div className="container grid grid--1x2 about-container">
@@ -49,19 +101,8 @@ class Home extends Component {
               <h2 className="about__heading">Heading</h2>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-                dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-                mollitia corporis?
-              </p>
-            </div>
-          </div>
-          
-          <div className="container grid grid--1x2 about-container">
-            <div className="about__content">
-              <h2 className="about__heading">Heading</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-                dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-                mollitia corporis?
+                dolorem cupiditate ex, assumenda omnis est incidunt repellat
+                sunt mollitia corporis?
               </p>
             </div>
           </div>
