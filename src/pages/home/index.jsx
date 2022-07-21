ae6fa91fb6d9d22c55faa7af5fa6cadfbfaffab8
Christian De Leon <christiandeleon12693@gmail.com>
2022-07-21 10:50:17 -0400

    finished intro block

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Infra1 from "../../assets/images/infra-1.png";
 import "./style.css";
 
 class Home extends Component {
@@ -18,40 +19,51 @@ class Home extends Component {
         </section>
 
         <section className="block">
-          <div className="container grid grid--1x2">
-            <h2 className="block__heading">Heading</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-              mollitia corporis?
-            </p>
+          <div className="container grid grid--1x2 about-container">
+            <div className="about__content">
+              <h2 className="about__heading">LET ME <span className="heading-highlight">INTRODUCE</span> MYSELF</h2>
+              <p>
+                I'm on a journey to master Cloud Computing, DevOps, and
+                Software Development.
+                <br />
+                <br />
+                My interest in cloud computing stemmed
+                from a desire to build large complex efficient systems. 
+                <br />
+                <br />
+                I believe in working above the baselivne of expectation by always
+                pushing myself to learn new and complex systems and
+                technologies.
+                <br />
+                <br />
+                Apart from Cloud Computing, I have a background in
+                Mechanical and Electrical Engineering as well as Software
+                Development.
+              </p>
+            </div>
+            <img src={Infra1} alt="" className="intro-image" />
           </div>
 
-          <div className="container grid grid--1x2">
-            <h2 className="block__heading">Heading</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-              mollitia corporis?
-            </p>
+          <div className="container grid grid--1x2 about-container">
+            <div className="about__content">
+              <h2 className="about__heading">Heading</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
+                mollitia corporis?
+              </p>
+            </div>
           </div>
-
-          <div className="container grid grid--1x2">
-            <h2 className="block__heading">Heading</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-              mollitia corporis?
-            </p>
-          </div>
-
-          <div className="container grid grid--1x2">
-            <h2 className="block__heading">Heading</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-              dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
-              mollitia corporis?
-            </p>
+          
+          <div className="container grid grid--1x2 about-container">
+            <div className="about__content">
+              <h2 className="about__heading">Heading</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+                dolorem cupiditate ex, assumenda omnis est incidunt repellat sunt
+                mollitia corporis?
+              </p>
+            </div>
           </div>
         </section>
       </React.Fragment>
