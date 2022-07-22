a2a6a04e2ce919614c7a28f9d9379b29b53819d4
Christian De Leon <christiandeleon12693@gmail.com>
2022-07-22 09:35:09 -0400

    added social links to navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
+import github from "../../assets/images/GitHub.png";
+import linkedin from "../../assets/images/Linkedin.png";
 import "./style.css";
 
 class NavBar extends Component {
@@ -10,12 +12,7 @@ class NavBar extends Component {
         <div className="nav-section">
           <nav className="container navbar">
             <Link to="/">
-              <img
-                alt=""
-                src={logo}
-                height="30px"
-                className="nav-brand"
-              />
+              <img alt="" src={logo} height="30px" className="nav-brand" />
             </Link>
             <ul className="list nav__list ">
               <li className="nav__item">
@@ -27,6 +24,24 @@ class NavBar extends Component {
               <li className="nav__item">
                 <Link to="/contact">Contact</Link>
               </li> */}
+              <span className="nav__item social-bg">
+                <a
+                  href="https://github.com/christian-deleon"
+                  target="_blank"
+                  className="social-link github"
+                >
+                  <img src={github} alt="" />
+                </a>
+              </span>
+              <span className="nav__item social-bg">
+                <a
+                  href="https://www.linkedin.com/in/christian-de-leon93/"
+                  target="_blank"
+                  className="social-link linkedin"
+                >
+                  <img src={linkedin} alt="" />
+                </a>
+              </span>
             </ul>
           </nav>
         </div>
