fd69a79305be6fb17b882b2a2109bbf62daf2eec
Christian De Leon <christiandeleon12693@gmail.com>
2022-07-21 23:16:27 -0400

    corrected some of the skill badges

--- src/pages/home/skills.js
+++ src/pages/home/skills.js
@@ -1,24 +1,23 @@
+
 const skills = [
-  {
-    name: "Bash",
-    svgName: "bash"
-  },
-  {
-    name: "Python",
-    svgName: "python"
-  },
-  {
-    name: "Git",
-    svgName: "git"
-  },
-  {
-    name: "VS Code",
-    svgName: "vscode"
-  },
-  {
-    name: "MySQL",
-    svgName: "mysql"
-  },
+  // { name: "Terraform",  svgName: "terraform" },
+  // { name: "Kubernetes", svgName: "kubernetes" },
+  { name: "Docker",     svgName: "docker" },
+  { name: "Bash",       svgName: "bash" },
+  { name: "Python",     svgName: "python" },
+  { name: "Git",        svgName: "git" },
+  { name: "GitHub",     svgName: "github" },
+  { name: "MySQL",      svgName: "mysql" },
+  { name: "VS Code",    svgName: "vscode" },
+  { name: "Selenium",    svgName: "selenium" },
+  { name: "JavaScript", svgName: "javascript" },
+  // { name: "TypeScript", svgName: "typescript" },
+  { name: "React",      svgName: "react" },
+  { name: "HTML 5",     svgName: "html-5" },
+  { name: "CSS 3",      svgName: "css3" },
+  { name: "Anaconda",   svgName: "anaconda" },
+  // { name: "Numpy",      svgName: "numpy" },
+  { name: "Pandas",     svgName: "pandas" },
 ];
 
 export function getSkills() {
