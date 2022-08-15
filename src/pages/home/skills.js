ce838ffe50cd294e2b1353c51731625066fe7011
Christian De Leon <christiandeleon12693@gmail.com>
2022-08-15 12:54:49 -0400

    updated skills

--- src/pages/home/skills.js
+++ src/pages/home/skills.js
@@ -1,6 +1,7 @@
 
 const skills = [
   { name: "Terraform",  svgName: "terraform",   level: "Proficient" },
+  { name: "Kubernetes", svgName: "kubernetes",  level: "Proficient" },
   { name: "Docker",     svgName: "docker",      level: "Proficient" },
   { name: "Python",     svgName: "python",      level: "Proficient" },
   { name: "Bash",       svgName: "bash",        level: "Proficient" },
@@ -8,7 +9,6 @@ const skills = [
   { name: "GitHub",     svgName: "github",      level: "Proficient" },
   { name: "MySQL",      svgName: "mysql",       level: "Proficient" },
   { name: "VS Code",    svgName: "vscode",      level: "Proficient" },
-  { name: "Kubernetes", svgName: "kubernetes",  level: "Intermediate" },
   { name: "Selenium",   svgName: "selenium",    level: "Intermediate" },
   { name: "JavaScript", svgName: "javascript",  level: "Intermediate" },
   { name: "TypeScript", svgName: "typescript",  level: "Intermediate" },
